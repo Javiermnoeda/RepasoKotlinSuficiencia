2ae1efca6d60a427abbe87b4cb84e01bbd6b8997
Javi <jmnoeda2000@gmail.com>
2021-03-29 12:26:07 +0200

    Commit

---
+++ src/5.Any.kt
@@ -0,0 +1,23 @@
+/*
+    El tipo Any es lo que Object a Java
+    Any es el supertipo de todos los tipos en kotlin
+ */
+
+
+fun main(){
+    //1. Crea distintas variables de tipo Any
+    val x: Any = 2
+    val nombre: Any = "Antonio"
+
+    //2. Crea distintas variables de tipo Any?
+    val y: Any? = 2
+    val apellidos: Any? = null
+
+    val numeros = listOf<Any>(15,"20",30.6,"cien")
+
+    /*
+        En definitiva Object es la super clase de todas las clases en Java, mientras que
+        Any es la super clase de todas las clases NO OPCIONALES en kotlin
+        Any? si queremos también usar el valor null
+     */
+}
